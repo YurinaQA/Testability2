62c8e22a9a988d699f6474ae85d6d454ae42027c
Test <irischj@mail.ru>
2024-02-27 23:06:19 +0500

    .gitignore

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+
+        int height = (int) 1.87; //м Рост
+        int weight = 98; //кг Вес
+
+        int bodyMassIndex = (int) service.calculate(height, weight);
+        System.out.println("Индекс массы тела = " + bodyMassIndex);
+    }
+}
